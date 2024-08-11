13dbef99f059d2d7440c31398be332c984695db2
MeeNordev <nordev.dev@gmail.com>
2024-08-12 00:30:07 +0300

    Changes by nikdekur

--- src/main/kotlin/com/mirage/mafiagame/module/Module.kt
+++
@@ -1,9 +0,0 @@
-package com.mirage.mafiagame.module
-
-import org.bukkit.plugin.java.JavaPlugin
-
-interface Module {
-    val app: JavaPlugin
-    fun onLoad()
-    fun onUnload()
-}
--- src/main/kotlin/com/mirage/mafiagame/task/TaskService.kt
+++ src/main/kotlin/com/mirage/mafiagame/task/TaskService.kt
@@ -3,21 +3,17 @@ package com.mirage.mafiagame.task
 import com.mirage.mafiagame.module.BaseModule
 import com.mirage.mafiagame.role.currentRole
 import com.mirage.mafiagame.task.type.InventoryTask
+import dev.nikdekur.minelib.plugin.ServerPlugin
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.plugin.java.JavaPlugin
 
-class TaskService(app: JavaPlugin) : BaseModule(app), Listener {
-    private val activeTasks = mutableMapOf<Player, Task>()
-
-    override fun onLoad() {
-        app.server.pluginManager.registerEvents(this, app)
-    }
+class TaskService(app: ServerPlugin) : BaseModule(app), Listener {
+    val activeTasks = hashMapOf<Player, Task>()
 
     override fun onUnload() {
-        InventoryClickEvent.getHandlerList().unregister(this)
         activeTasks.clear()
     }
 
@@ -35,6 +31,7 @@ class TaskService(app: JavaPlugin) : BaseModule(app), Listener {
     }
 
     fun Player.isTaskAvailable(task: Task): Boolean {
+        // TODO: Change complexity to O(1) from 2 x O(n)
         return this.currentRole?.name in task.taskFor.map { it.name }
     }
 }
